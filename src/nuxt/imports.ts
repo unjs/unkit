dd444fcb0137343ef8c2aab6b21792aac4d2f683
Inesh Bose <56732164+ineshbose@users.noreply.github.com>
2023-02-01 14:04:54 +0000

    chore: first implementation of nuxt module, yet to look into build

---
+++ src/nuxt/imports.ts
@@ -0,0 +1,8 @@
+export * as env from '../env' // 'unkit/env' or '~/src/env'
+export * as esm from '../esm'
+export * as fetch from '../fetch'
+export * as http from '../http'
+export * as object from '../object'
+export * as promise from '../promise'
+export * as string from '../string'
+export * as url from '../url'
